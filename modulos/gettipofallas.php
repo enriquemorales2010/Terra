d7545e9b69c1e97312b747728837bcae2dd27bbc
enriquemorales2010 <emc@blcorporations.com>
2026-07-06 21:35:03 -0300

    Fix dynamic falla element loading and update editar_postventa compatibility

--- modulos/gettipofallas.php
+++ modulos/gettipofallas.php
@@ -1,24 +1,13 @@
-<?php  
-require_once '../clases/postventas.php';
+<?php
+require_once __DIR__ . '/../clases/falla.php';
 
-// Verificar que llegaron todos los datos
-if(isset($_POST['id_falla']) && isset($_POST['caso']) && isset($_POST['descripcion'])) {
-    
-    $falla = $_POST['id_falla'];
-    $caso = $_POST['caso'];
-    $descripcion = $_POST['descripcion']; // <-- ESTABA FALTANDO
-    $decision = 0;
-    $costo = 0;
-
-    $con = new postventas();
-    
-    // Pasar la descripción a la función
-    $con->agregarfal($falla, $caso, $decision, $costo, $descripcion);
-    
-    // Redireccionar de vuelta al detalle del caso
-    header('Location: ../detalle_caso_postventas.php?num_caso=' . $caso . '&mensaje=Falla agregada correctamente');
-} else {
-    // Si faltan datos, redireccionar con error
-    header('Location: ../detalle_caso_postventas.php?num_caso=' . $_POST['caso'] . '&error=Faltan datos requeridos');
+if (!isset($_POST['clase_falla'])) {
+    echo "<option value=''>Seleccione una clase de falla primero</option>";
+    exit;
 }
+
+$clase_falla = intval($_POST['clase_falla']);
+
+$con = new fallas();
+$con->buscar($clase_falla);
 ?>
